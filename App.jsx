2334e2d4844bde73782275fe8be337830e9f450f
Narendra <narendra@Narendras-MacBook-Pro.local>
2017-07-30 05:52:41 +0530

    on button click how we can update data in state

--- App.jsx
+++ App.jsx
@@ -6,55 +6,32 @@ class App extends React.Component {
     this.state={
         data:
         [
-            { 
-                "id":1,
-                "name":"Bhavin",
-                "age":"30"
-
-            },
-            {
-                "id":2,
-                "name":"Tejas",
-                "age":"25"
-            },
-            {
-                "id":3,
-                "name":"Vishal",
-                "age":"32"
-            }
-
+            
         ]
 
         
     }
+    this.setStateHandler= this.setStateHandler.bind(this);
    }
-   render() {
-       var i=1;
 
-    var myStyle = {
-         fontSize: 50,
-         color: '#FF0000'
-      }
+   setStateHandler(){
+       var item="set state.."
+       var myArray=this.state.data;
+       myArray.push(item);
+       this.setState({data:myArray})
+   };
+
+
+   render() {
+      
       return (
          <div> 
-            Hello World!!! Narendr
-            {// in line Comment
-            }
-            {/* Multi line comment
-            Best practises : 
-            1. html tag should always using localcase tag name
-            2. React component always start with Upper case
-            3. To specify class in html tag we should use classsName & htmlFor
+          
 
-
-
-             */}
-            <Header/>
-            <table>
-                <tbody>
-                    {this.state.data.map((person,i)=><TableRow key={i} data={person}/>)}
-                </tbody>
-            </table>
+          
+           
+            <button onClick={this.setStateHandler}>Add state</button>
+            <h4>{this.state.data}</h4>    
          </div>
       );
    }
@@ -68,18 +45,6 @@ class Header extends React.Component {
       );
    }
 }
-class TableRow extends React.Component{
 
-    render(){
-        return(
-            <tr>
-                <td>{this.props.data.id}</td>
-                <td>{this.props.data.name}</td>
-                <td>{this.props.data.age}</td>
-            </tr>
-        );
-
-    }
-}
 
 export default App;
